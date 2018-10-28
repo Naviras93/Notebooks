4bce5d9237f4ecd8fc56e84b5c2d83ad9ec06eb2
Ivan <Ivan.Saric.WIN14G@utb.hermods.se>
2018-10-28 16:43:06 +0100

    notebooks can be created from the first page wowie

--- src/main/java/jsf/NotebookBean.java
+++ src/main/java/jsf/NotebookBean.java
@@ -13,17 +13,49 @@ import java.util.List;
 @ManagedBean
 @SessionScoped
 public class NotebookBean {
+    private String createNotebookName;
     private List<Notebook> notebooks;
     private List<Chapter> chapters;
+    private Notebook selectedNotebook;
 
     @EJB
     NotebookService notebookService;
 
-    public void createNotebook(String name){
+    public void createNotebook(){
         Notebook nb = new Notebook();
-        nb.setName(name);
+        nb.setName(createNotebookName);
         notebookService.createNotebook(nb);
     }
 
+    public List<Notebook> getNotebooks() {
+        return notebooks;
+    }
 
+    public void setNotebooks(List<Notebook> notebooks) {
+        this.notebooks = notebooks;
+    }
+
+    public void updateNotebook() {
+        selectedNotebook = new Notebook();
+    }
+
+    public void deleteNotebook() {
+        notebooks.remove(selectedNotebook);
+    }
+
+    public List<Chapter> getChapters() {
+        return chapters;
+    }
+
+    public void setChapters(List<Chapter> chapters) {
+        this.chapters = chapters;
+    }
+
+    public String getCreateNotebookName() {
+        return createNotebookName;
+    }
+
+    public void setCreateNotebookName(String createNotebookName) {
+        this.createNotebookName = createNotebookName;
+    }
 }
